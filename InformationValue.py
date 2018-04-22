90ffebea5e7d5cffa64844b9e3b35ba80f76c5b3
zmx <zmx@zmxdeMacBook.local>
2018-04-22 21:49:39 +0800

    180422

--- InformationValue.py
+++ InformationValue.py
@@ -1,7 +1,7 @@
-from sklearn.utils.multiclass import type_of_target
+import math
 import numpy
 from scipy import stats
-import math
+from sklearn.utils.multiclass import type_of_target
 
 
 class InformationValue:
@@ -12,46 +12,44 @@ class InformationValue:
     def check_binary(self,y):
         if type_of_target(y) != 'binary': raise ValueError('Class must be binary')
 
-    def discrete_feature(self,x,discrete_count=None):
+    def discrete_continuous_feature(self,x,v):
+        m = len(x)
+        _x = numpy.zeros(m)
+        n = len(v)-1
+        for i in range(n):
+            p = v[i]
+            q = v[i+1]
+            _x[numpy.where((x >= p) & (x < q))] = i
+        return _x
+
+    def discrete_feature(self,x,v):
         n,m = x.shape
         _x = numpy.zeros((n,m))
         for i in range(m):
             xx = x[:,i]
-            if type_of_target(xx) == 'continuous':
-                if discrete_count is None:
-                    _x[:,i] = self.discrete_continuous_feature(xx)
-                else:
-                    _x[:,i] = self.discrete_continuous_feature(xx,discrete_count.get(i,5))
+            if type_of_target(xx) == 'continuous': _x[:,i] = self.discrete_continuous_feature(xx,v[i])
             else: _x[:,i] = xx
         return _x
 
-    def discrete_continuous_feature(self,x,n=5):
-        m = len(x)
-        _x = numpy.zeros(m)
-        for i in range(n):
-            p1 = stats.scoreatpercentile(x,100/float(n)*i)
-            p2 = stats.scoreatpercentile(x,100/float(n)*(i+1))
-            _x[numpy.where((x >= p1) & (x <= p2))] = i
-        return _x
+    def count_value(self,y):
+        return len(y[y == 0]),len(y[y == 1])
 
-    def get_woe_iv(self,x,y,discrete_count=None):
+    def get_woe_iv(self,x,y,v):
         self.check_binary(y)
-        total_n0,total_n1 = self.value_count(y)
-
-        _x = self.discrete_feature(x,discrete_count)
+        n0,n1 = self.count_value(y)
+        _x = self.discrete_feature(x,v)
         n,m = x.shape
         woe = []
         iv = []
-
         for i in range(m):
-            xx = _x[:,i]
-            _xx = numpy.unique(xx)
             _woe = {}
             _iv = 0
-            for v in _xx:
-                vn0,vn1 = self.value_count(y[numpy.where(xx == v)])
-                r0 = float(vn0)/total_n0
-                r1 = float(vn1)/total_n1
+            xx = _x[:,i]
+            ux = numpy.unique(xx)
+            for v in ux:
+                _n0,_n1 = self.count_value(y[numpy.where(xx == v)])
+                r0 = float(_n0)/n0
+                r1 = float(_n1)/n1
                 if r0 == 0: _woe[v] = self.max_woe
                 elif r1 == 0: _woe[v] = self.min_woe
                 else: _woe[v] = math.log(r1/r0)
@@ -60,19 +58,31 @@ class InformationValue:
             iv.append(_iv)
         return woe,iv
 
-    def value_count(self,y):
-        n0 = len(y[y == 0])
-        n1 = len(y[y == 1])
-        return n0,n1
-
 
 if __name__ == '__main__':
+    def get_feature_range(x):
+        n,m = x.shape
+        v = {}
+        for i in range(m):
+            xx = x[:,i]
+            if type_of_target(xx) == 'continuous':
+                v[i] = [
+                    -numpy.inf,
+                    stats.scoreatpercentile(xx, 20),
+                    stats.scoreatpercentile(xx, 40),
+                    stats.scoreatpercentile(xx, 60),
+                    stats.scoreatpercentile(xx, 80),
+                    numpy.inf
+                ]
+        return v
+
+
     import pandas
-    from sklearn.preprocessing import Imputer
     data = pandas.read_csv('cs-training.csv')
     y = data['SeriousDlqin2yrs'].values
-    x = data.drop(['ID', 'SeriousDlqin2yrs'], axis=1).values
+    x = data.drop(['ID','SeriousDlqin2yrs'],axis=1).values
+    from sklearn.preprocessing import Imputer
     imputer = Imputer()
     x = imputer.fit_transform(x)
-    woe, iv = InformationValue().get_woe_iv(x,y)
+    woe,iv = InformationValue().get_woe_iv(x,y,get_feature_range(x))
     print(numpy.array(iv))
